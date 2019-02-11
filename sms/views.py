d65f673d5150a3fe2b45e87748c964830e657ba4
groovecoder <luke.crouch@gmail.com>
2019-02-10 21:16:31 -0600

    use oscn to find case and arraignment

--- sms/views.py
+++ sms/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 
+import oscn
 from twilio.twiml.messaging_response import MessagingResponse
 
 
 @csrf_exempt
 def twilio(request):
     resp = MessagingResponse()
-    resp.message("Thanks for the message")
-
+    case_num = request.POST['Body']
+    case = oscn.request.Case(year='2018', number=case_num)
+    for event in case.events:
+        if "arraignment" in event.Docket.lower():
+            resp.message("The arraignment is %s" % event.Event)
+            return HttpResponse(resp)
+    resp.message("Could not find arraignment for case %s " % case_num)
     return HttpResponse(resp)
