8f51003b40528a483094c488b8d95aaabd50301f
groovecoder <luke.crouch@gmail.com>
2019-07-13 13:56:02 -0500

    add whitenoise to serve statics

--- courtbot/settings.py
+++ courtbot/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'courtbot.urls'
