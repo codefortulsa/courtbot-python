8cd36d098bb43f0c91caf86ba18927ffedd46e00
Maxx Crawford <maxx.crawford@gmail.com>
2019-07-13 11:21:36 -0500

    Defined base/index template files

--- website/views.py
+++ website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    # """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html')
