4d9f409cea3ce253b5b233c6dd47f0e1dcbe1d75
groovecoder <luke.crouch@gmail.com>
2019-11-05 19:50:33 -0600

    use RequestFactory for tests

--- api/tests.py
+++ api/tests.py
@@ -1,54 +1,53 @@
-from django.test import TestCase
-from api.views import reminders
 from ast import literal_eval
 from datetime import datetime, timedelta
+
+from django.test import RequestFactory, TestCase
+
+from .views import reminders
 from alerts.models import Alert
 
-class testReminders(TestCase):
-    class Req():
-        def __init__(self, arraignment_datetime, case_num, phone_num):
-            self.POST = {
-                "arraignment_datetime": arraignment_datetime,
-                "case_num": case_num,
-                "phone_num": phone_num
-            }
 
+class testReminders(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def _post(self, url, params):
+        return self.factory.post(url, params)
 
     def testReminderWithArraignmentIn8Days(self):
         arraignment_datetime = (datetime.today() + timedelta(days=8)).strftime('%Y-%m-%dT%H:%M:%S')
-        request = self.Req(
-            arraignment_datetime,
-            1,
-            "+1-918-555-5555"
-        )
+        request = self._post('/api/reminders', {
+            "arraignment_datetime": arraignment_datetime,
+            "case_num": 1,
+            "phone_num": "+1-918-555-5555",
+        })
         result = reminders(request)
         response = literal_eval(result.content.decode('utf-8'))
         self.assertEqual(Alert.objects.all().count(), 2)
-        self.assertEqual(response['status'], '201 Created') 
+        self.assertEqual(response['status'], '201 Created')
 
 
     def testReminderWithArraignmentIn2Days(self):
         arraignment_datetime = (datetime.today() + timedelta(days=2)).strftime('%Y-%m-%dT%H:%M:%S')
-        request = self.Req(
-            arraignment_datetime,
-            2,
-            "+1-918-555-5555"
-        )
+        request = self._post('/api/reminders', {
+            "arraignment_datetime": arraignment_datetime,
+            "case_num": 2,
+            "phone_num": "+1-918-555-5555",
+        })
         result = reminders(request)
         response = literal_eval(result.content.decode('utf-8'))
         self.assertEqual(Alert.objects.all().count(), 1)
-        self.assertEqual(response['status'], '201 Created') 
+        self.assertEqual(response['status'], '201 Created')
 
 
     def testReminderWithArraignmentToday(self):
         arraignment_datetime = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
-        request = self.Req(
-            arraignment_datetime,
-            3,
-            "+1-918-555-5555"
-        )
+        request = self._post('/api/reminders', {
+            "arraignment_datetime": arraignment_datetime,
+            "case_num": 3,
+            "phone_num": "+1-918-555-5555",
+        })
         result = reminders(request)
         response = literal_eval(result.content.decode('utf-8'))
         self.assertEqual(Alert.objects.all().count(), 0)
-        self.assertEqual(response['status'], '410 Gone') 
-        
+        self.assertEqual(response['status'], '410 Gone')
