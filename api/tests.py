7c46b019bbf3451023e03674fc31323b73bd8346
Kendall Werts <kmwerts@gmail.com>
2019-11-09 10:57:22 -0600

    fix for #42: added eligible juridiction endpoint

--- api/tests.py
+++ api/tests.py
@@ -3,7 +3,7 @@ import json
 
 from django.test import RequestFactory, TestCase
 
-from .views import reminders
+from .views import reminders, eligible_jurisdiction
 from alerts.models import Alert
 
 
@@ -13,6 +13,9 @@ class testReminders(TestCase):
 
     def _post(self, url, params):
         return self.factory.post(url, params)
+
+    def _get(self, url, params):
+        return self.factory.get(url, params)
 
     def testReminderWithArraignmentIn8Days(self):
         arraignment_datetime = (datetime.today() + timedelta(days=8)).strftime('%Y-%m-%dT%H:%M:%S')
@@ -26,7 +29,6 @@ class testReminders(TestCase):
         self.assertEqual(Alert.objects.all().count(), 2)
         self.assertEqual(resp_json['status'], '201 Created')
 
-
     def testReminderWithArraignmentIn2Days(self):
         arraignment_datetime = (datetime.today() + timedelta(days=2)).strftime('%Y-%m-%dT%H:%M:%S')
         request = self._post('/api/reminders', {
@@ -39,7 +41,6 @@ class testReminders(TestCase):
         self.assertEqual(Alert.objects.all().count(), 1)
         self.assertEqual(resp_json['status'], '201 Created')
 
-
     def testReminderWithArraignmentToday(self):
         arraignment_datetime = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
         request = self._post('/api/reminders', {
@@ -51,7 +52,6 @@ class testReminders(TestCase):
         resp_json = json.loads(response.content)
         self.assertEqual(Alert.objects.all().count(), 0)
         self.assertEqual(resp_json['status'], '410 Gone')
-   
 
     def testPreventDuplicateReminder(self):
         arraignment_datetime = (datetime.today() + timedelta(days=8)).strftime('%Y-%m-%dT%H:%M:%S')
@@ -59,7 +59,7 @@ class testReminders(TestCase):
             "arraignment_datetime": arraignment_datetime,
             "case_num": 1,
             "phone_num": "+1-918-555-5555",
-        })       
+        })
         response = reminders(request)
         resp_json = json.loads(response.content)
         self.assertEqual(Alert.objects.all().count(), 2)
@@ -67,3 +67,12 @@ class testReminders(TestCase):
         reminders(request) # Submitting a duplicate reminder
         self.assertEqual(Alert.objects.all().count(), 2)
         self.assertEqual(resp_json['status'], '201 Created')
+
+    def testEligibleJurisdictions(self):
+        request = self._get('/api/eligible_jurisdiction', {
+            "state": 'OK'
+        })
+        response = eligible_jurisdiction(request)
+        resp_json = json.loads(response.content)
+        print(resp_json)
+        self.assertEqual(resp_json, {'jurisdiction_type': 'county', 'eligible_jurisdictions': ['Tulsa', 'Rogers']})
--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('case', views.case, name='case'),
     path('reminders', views.reminders, name='reminders'),
+    path('eligible-jurisdiction', views.eligible_jurisdiction, name='eligible_jurisdiction'),
 ]
