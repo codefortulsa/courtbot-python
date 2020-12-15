d5c6830e04fcddcd855adeb2ee16503453e890dc
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2020-12-14 20:06:47 -0600

    change backend only

--- website/views.py
+++ website/views.py
@@ -19,15 +19,11 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html')
 
-def check_valid_case(request):
+def check_valid_case(case_num, year, county):
     # Process form data and requests arraignment data form api/case
-    case_num = request.POST['case_num']
-    year = request.POST['year']
-    county = request.POST['county']
-    phone_num = request.POST['phone_num']
-    add_num = request.POST['add_phone_num']
     resp = requests.get(
-        f"https://courtbot-python.herokuapp.com/api/case?year={year}&county={county}&case_num={case_num}"
+        #f"https://courtbot-python.herokuapp.com/api/case?year={year}&county={county}&case_num={case_num}"
+        f"http://127.0.0.1:8000/api/case?year={year}&county={county}&case_num={case_num}"
     )
     resp_json = json.loads(resp.content)
     if resp_json.get('error', None):
@@ -36,7 +32,8 @@ def check_valid_case(request):
 
 
 def set_case_reminder(arraignment_datetime, case_num, phone_num):
-    reminder_request = requests.post('https://courtbot-python.herokuapp.com/api/reminders', {
+    #reminder_request = requests.post('https://courtbot-python.herokuapp.com/api/reminders', {
+    reminder_request = requests.post('http://127.0.0.1:8000/api/reminders', {
         "arraignment_datetime": arraignment_datetime,
         "case_num": case_num,
         "phone_num": f"+1-{phone_num}"
@@ -52,25 +49,31 @@ def set_case_reminder(arraignment_datetime, case_num, phone_num):
 def schedule_reminders(request):
     # If valid case and arraignment time, posts reminder data to api/reminder
     # Includes option for extra phone number for additional recipient
-    case_num = request.POST['case_num']
+    #case_num = request.POST['case_num']
+    case_num_list = [ 
+            value for key, value in request.POST.items() 
+            if key.find("case_num") > -1 and value
+        ]
+    year = request.POST['year']
+    county = request.POST['county']
     phone_num = request.POST['phone_num']
     add_num = request.POST.get('add_phone_num', None)
-
-    valid_case_message, arraignment_datetime = check_valid_case(request)
-    if not arraignment_datetime:
-        messages.error(request, valid_case_message)
-        faq_message = (
-            f'Please check the case for further information using steps provided at http://court.bot/#faq'
-        )
-        messages.error(request, faq_message)
-    else:
-        reminder_set, reminder_message = set_case_reminder(arraignment_datetime, case_num, phone_num)
-        # messages.error(request, message)          
-        messages.info(request, reminder_message)
-        if not reminder_set:
-        	return redirect('/#form')
-        if add_num:
-          _, another_reminder_message = set_case_reminder(arraignment_datetime, case_num, add_num)
-          messages.info(request, another_reminder_message)
+    for case_num in case_num_list:
+        valid_case_message, arraignment_datetime = check_valid_case(case_num, year, county)
+        if not arraignment_datetime:
+            messages.error(request, valid_case_message)
+            faq_message = (
+                f'Please check the case for further information using steps provided at http://court.bot/#faq'
+            )
+            messages.error(request, faq_message)
+        else:
+            reminder_set, reminder_message = set_case_reminder(arraignment_datetime, case_num, phone_num)
+            # messages.error(request, message)          
+            messages.info(request, reminder_message)
+            if not reminder_set:
+                return redirect('/#form')
+            if add_num:
+                _, another_reminder_message = set_case_reminder(arraignment_datetime, case_num, add_num)
+                messages.info(request, another_reminder_message)
     return redirect('/#form')
 
